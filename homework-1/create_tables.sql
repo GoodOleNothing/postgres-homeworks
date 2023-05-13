e47cfbe2b1f6e513549272e92e477378362c6368
GoodOleNothing <alexgog77777@gmail.com>
2023-05-14 00:45:02 +0500

    Запись создания твблиц в create_tables.sql сделана, данные из файлов в таблици зыписываются.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	first_name varchar(100),
+	last_name varchar(100),
+	title text,
+	birth_date date,
+	notes text
+);
+SELECT * FROM employees;
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+SELECT * FROM customers;
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int,
+	order_date date,
+	ship_city varchar(100)
+);
+SELECT * FROM orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,53 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+# подключаемся к БД
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='1303'
+)
+
+cur = conn.cursor()
+
+# работаем с таблицей employees
+with open('north_data/employees_data.csv', 'r') as f:
+    ed = csv.reader(f)
+    for row in ed:
+        cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)', tuple(row))
+cur.execute('SELECT * FROM employees')
+rows = cur.fetchall()
+#for row in rows:
+#    print(row)
+conn.commit()
+
+# работаем с таблицей customers
+with open('north_data/customers_data.csv', 'r') as f:
+    cd = csv.reader(f)
+    for row in cd:
+        cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', tuple(row))
+cur.execute('SELECT * FROM customers')
+rows = cur.fetchall()
+#for row in rows:
+#    print(row)
+conn.commit()
+
+# работаем с таблицей orders
+with open('north_data/orders_data.csv', 'r') as f:
+    od = csv.reader(f)
+    for row in od:
+        cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', tuple(row))
+cur.execute('SELECT * FROM orders')
+rows = cur.fetchall()
+#for row in rows:
+#    print(row)
+conn.commit()
+
+cur.close()
+conn.close()
+
+# emp: "first_name","last_name","title","birth_date","notes"
+# cust: "customer_id","company_name","contact_name"
+# ord: "order_id","customer_id","employee_id","order_date","ship_city"
